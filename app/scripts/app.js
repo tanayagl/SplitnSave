cc02ac573beb83c741cc30bfdfd00acd42232693
Tanay agrawal <Tanay@Tanays-MacBook-Pro.local>
2016-04-04 16:15:54 +0530

    Nav bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,29 +9,34 @@
  * Main module of the application.
  */
 angular
-  .module('splitnSaveApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('splitnSaveApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/homepage.html',
+                controller: 'HomepageCtrl',
+                controllerAs: 'homepage'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .when('/books', {
+                templateUrl:'views/books.html',
+                controller:'BooksCtrl',
+                controllerAs:'books',
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
